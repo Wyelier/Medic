a9cb51a87b01215d6c5c95f92e79352429cec7e5
Wyelier <sleptsov2004@gmail.com>
2023-05-05 00:50:44 +0900

    2 session

--- app/src/main/java/com/example/medicmadskill/Api/MedicAPI.java
+++ app/src/main/java/com/example/medicmadskill/Api/MedicAPI.java
@@ -7,14 +7,5 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface MedicAPI {
-    @POST("api/sendCode")
-    Call<Medic>
-
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://virtserver.swaggerhub.com/serk87/APIfood/FRBHWRIOJAFIDSNKJF")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
-
-
 
 }
