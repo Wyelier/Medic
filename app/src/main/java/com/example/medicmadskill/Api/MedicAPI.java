08794ecc48431a8b234d2223334ce77222d94acb
sleptsovigor <sleptsov2004@gmail.com>
2023-05-05 13:47:47 +0900

    3 session

--- app/src/main/java/com/example/medicmadskill/Api/MedicAPI.java
+++ app/src/main/java/com/example/medicmadskill/Api/MedicAPI.java
@@ -1,11 +1,5 @@
 package com.example.medicmadskill.Api;
 
-import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-
 public interface MedicAPI {
 
 }
--- app/src/main/java/com/example/medicmadskill/PassActivity.java
+++ app/src/main/java/com/example/medicmadskill/PassActivity.java
@@ -1,6 +1,8 @@
 package com.example.medicmadskill;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,14 @@ public class PassActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pass_activity);
+
+        TextView textView = findViewById(R.id.textSkip);
+
+        textView.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PatientActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
